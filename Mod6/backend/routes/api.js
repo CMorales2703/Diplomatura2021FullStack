0aeb83f6bddbad0f20712f973c886d901c049a50
Cristian Ariel Morales <cristian.a.morales@gmail.com>
2021-11-24 21:59:12 -0300

    sendMail

--- Mod6/backend/routes/api.js
+++ Mod6/backend/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
+
 
 router.get('/novedades', async function (req, res, next) {
 
@@ -27,6 +29,32 @@ router.get('/novedades', async function (req, res, next) {
   });
  
   res.json(novedades);
+});
+
+router.post('/contacto', async (req, res) => {
+  const mail = {
+    to: 'cristian.a.morales@gmail.com',
+    subject: 'Contacto Web',
+    html: `${req.body.nombre} se contacto a traves de la web y quiere mas informacion a este correo: ${req.body.email} <br> Ademas, hizo el siguiente comentario: ${req.body.mensaje} <br> su tel es: ${req.body.telefono}`
+  }
+
+  const transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+    
+  });
+
+  await transport.sendMail(mail)
+
+  res.status(201).json({
+    error: false,
+    message: 'Mensaje Enviado'
+  });
+
 });
 
 module.exports = router;
--- Mod6/frontend/src/pages/contactoPage.js
+++ Mod6/frontend/src/pages/contactoPage.js
@@ -1,30 +1,66 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import '../styles/components/pages/contactoPage.css';
 
-const contactoPage = (props) => {
+const ContactoPage = (props) => {
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState('');
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
     return(
         <main className="holder">
          <div className="columna left">
              <h2>Contacto Rapido</h2>
-             <form action="" method="" className="formulario">
+             <form action="/contacto" method="post" className="formulario" onSubmit={handleSubmit}>
                  <p>
                      <label>Nombre</label>
-                     <input type="text" name="nombre"/>
+                     <input type="text" name="nombre" value={formData.nombre} onChange={handleChange}/>
                  </p>
                  <p>
                     <label>Email</label>
-                    <input type="text" name="email"/>
+                    <input type="text" name="email" value={formData.email} onChange={handleChange}/>
                 </p>
                 <p>
                     <label>Telefono</label>
-                    <input type="text" name="telefono"/>
+                    <input type="text" name="telefono" value={formData.telefono} onChange={handleChange}/>
                 </p>
                 <p>
                     <label>Mensaje</label>
-                    <textarea name="mensaje"></textarea>
+                    <textarea name="mensaje" value={formData.mensaje} onChange={handleChange}></textarea>
                 </p>
                 <p className="acciones">
                     <input type="submit" value="Enviar"/>
                 </p>
+                {sending ? <p>Enviando...</p> : null}
+                {msg ? <p>{msg}</p> : null}
              </form>
          </div>
          <div className="columna right">
@@ -42,4 +78,4 @@ const contactoPage = (props) => {
     )
 }
 
-export default contactoPage;
+export default ContactoPage;
